48ee908511ccdc45bc5c62c20502b3418b16660e
BangBoss25 <tiganareymansyah2502@gmail.com>
2023-01-17 18:10:34 +0700

    nambah file pertemuan 5

---
+++ Pertemuan5.js
@@ -0,0 +1,17 @@
+// Cara clone repository di git hub
+// 1. Buka github
+// 2. Cari repo yang ingin di clone dan salin urlnya
+// 3. Buka terminal di cd yang kita inginkan
+// 4. Ketik perintah git clone urlnya dan pastikan terhubung ke internet
+// 5. Jika itu project node js maka ketik perintah npm i di cmd vscode nya
+
+// Cara mengubah nama repository yang di fork
+// 1. Ubah di remote / gihub
+//      - Buka github dan buka reponya
+//      - Pilih setting
+//      - Klik rename dan ubah nama reponya
+// 2. Ubah di local / komputer
+//      - Buka terminal
+//      - Ketikkan perintah git remote set-url origin urlrepoyangdiubahtadi
+//      - Jika ingin menceknya ketikkan perintah git remote -v
+//      - Jika sudah sama lalu renamelah folder yang di fork
